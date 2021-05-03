3ea043919713798a7df7cb321429b24bd0397d03
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-05-03 20:59:26 -0300

    Agrego repositorio para los usuarios

--- src/main/java/servicios/repositorios/RepositorioUsuarios.java
+++ src/main/java/servicios/repositorios/RepositorioUsuarios.java
@@ -1,4 +1,24 @@
 package servicios.repositorios;
 
-public interface RepositorioUsuarios {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import modelo.Usuario;
+
+public class RepositorioUsuarios {
+	private static RepositorioUsuarios repositorioUsuarios;
+	private List<Usuario> listaUsuarios = new ArrayList<>();
+
+	public static RepositorioUsuarios getInstance() {
+		if(repositorioUsuarios == null) {
+			repositorioUsuarios = new RepositorioUsuarios();
+		}
+		return repositorioUsuarios;
+	}
+
+	public void agregarUsuario(Usuario usuario) {
+		listaUsuarios.add(usuario);
+	}
+
 }
