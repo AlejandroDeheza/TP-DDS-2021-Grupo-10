17cb8e46445a1c9f6d1ff6a0cfdd1fa70f9cfbeb
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-05-03 21:02:34 -0300

    Agrego herencia del DuenioMascota

--- src/main/java/modelo/DuenioMascota.java
+++ src/main/java/modelo/DuenioMascota.java
@@ -1,23 +1,31 @@
 package modelo;
 
+import excepciones.DuenioMascotaMascotaRegistradaException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class DuenioMascota extends Usuario{
 
-  private List<Mascota> mascotas;
+	private List<Mascota> listaMascotas;
 
-  public DuenioMascota(String usuario, String contrasenia, Persona duenioMascota, List<Mascota> mascotas){
-    super(usuario, contrasenia, duenioMascota);
-    this.mascotas = mascotas;
-  }
+	public DuenioMascota(String usuario, String contrasenia, Persona duenioMascota, List<Mascota> listaMascotas){
+		super(usuario, contrasenia, duenioMascota);
+		this.listaMascotas = listaMascotas;
+	}
 
-  public void agregarMascota(Mascota mascota){
-    if(this.mascotas == null){this.mascotas = new ArrayList<>();}
-    this.mascotas.add(mascota);
-  }
+	public void agregarMascota(Mascota mascota){
+		validarExistenciaMascota(mascota);
+		this.listaMascotas.add(mascota);
+	}
 
-  public void eliminarMascota(Mascota mascota){
-    this.mascotas.remove(mascota);
-  }
+	public void eliminarMascota(Mascota mascota){
+		this.listaMascotas.remove(mascota);
+	}
+
+	private void validarExistenciaMascota(Mascota mascota) {
+		if(this.listaMascotas.contains(mascota)){
+			throw new DuenioMascotaMascotaRegistradaException("Ya tiene registrada la mascota.")
+		}
+	}
 }
