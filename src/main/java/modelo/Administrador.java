07b3cfbe615e1196e5d63ea978bd31e064913aa2
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-05-03 21:03:03 -0300

    Agrego herencia del Administrador

--- src/main/java/modelo/Administrador.java
+++ src/main/java/modelo/Administrador.java
@@ -1,8 +1,22 @@
 package modelo;
 
+<<<<<<< HEAD
 public class Administrador extends Usuario{
 
   public Administrador(String usuario, String contrasenia, Persona persona){
     super(usuario, contrasenia, persona);
   }
+=======
+import excepciones.DuenioMascotaMascotaRegistradaException;
+
+import java.util.List;
+
+public class Administrador extends Usuario {
+	public Administrador(String username, String password, Persona persona) {
+		super(username, password, persona);
+	}
+	public void agregarCaracteristica(Caracteristica caracteristica){
+		// TODO: Ver como agregar caracteristica
+	}
+>>>>>>> 786d5af (Agrego herencia del Administrador)
 }
