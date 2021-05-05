800dc7afccb632114de19045c6942d1071c86283
AlejandroDeheza <ale.deheza@gmail.com>
2021-05-05 17:44:47 -0300

    Agrego metodos para agregar y remover mascotas

--- src/main/java/modelo/DuenioMascota.java
+++ src/main/java/modelo/DuenioMascota.java
@@ -1,5 +1,6 @@
 package modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DuenioMascota extends Usuario{
@@ -9,5 +10,14 @@ public class DuenioMascota extends Usuario{
   public DuenioMascota(String usuario, String contrasenia, Persona duenioMascota, List<Mascota> mascotas){
     super(usuario, contrasenia, duenioMascota);
     this.mascotas = mascotas;
+  }
+
+  public void agregarMascota(Mascota mascota){
+    if(this.mascotas == null){this.mascotas = new ArrayList<>();}
+    this.mascotas.add(mascota);
+  }
+
+  public void eliminarMascota(Mascota mascota){
+    this.mascotas.remove(mascota);
   }
 }
