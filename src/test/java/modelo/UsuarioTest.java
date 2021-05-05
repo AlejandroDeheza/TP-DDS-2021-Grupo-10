1d26c7f01529435e3c1ea499dbf866712decfa37
maxi09 <marzm06@gmail.com>
2021-05-05 00:33:35 -0300

    Creacion de tests y excepcion

---
+++ src/test/java/modelo/UsuarioTest.java
@@ -0,0 +1,48 @@
+package modelo;
+
+import excepciones.DatosDeContactoIncompletos;
+import excepciones.LoginInvalidoException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class UsuarioTest {
+
+  private static DuenioMascota pablo;
+  private static Persona pabloPersona;
+  private static DatosDeContacto datosPablo;
+  private static Persona damianPersona;
+  private static DatosDeContacto datosDamian;
+  private static Administrador damian;
+
+  @BeforeAll
+  public static void contextLoad() {
+    datosPablo = new DatosDeContacto(null, "pablo@mail.com");
+    pabloPersona = new Persona("pablo", "Hernandez", TipoDocumento.DNI, "43212098", datosPablo, LocalDate.of(1995, 8, 7) );
+    pablo =new DuenioMascota("pepe2","P3p3.3210",pabloPersona);
+    datosDamian = new DatosDeContacto(1143091234, null);
+    damianPersona = new Persona("damian", "perez", TipoDocumento.DNI, "49102921", datosDamian, LocalDate.of(2001,4,1) );
+    damian = new Administrador("damian", "Awqde1oP", damianPersona);
+  }
+
+  @Test
+  public void testCrearDosUsuariosMismoLogin() {
+    Assertions.assertThrows(LoginInvalidoException.class,()->new DuenioMascota("pepe2","P3p3.3210",pabloPersona));
+  }
+
+
+  @Test
+  public void testCrearUsuarioAdministrador(){
+
+    Assertions.assertSame(Administrador.class, damian.getClass());
+
+  }
+
+  @Test
+  public void testCrearSinDatoDeContacto() {
+    Assertions.assertThrows(DatosDeContactoIncompletos.class,()->new Persona(null, null, TipoDocumento.DNI, "43212098", new DatosDeContacto(null, null), LocalDate.of(1995, 8, 7) ));
+  }
+
+}
