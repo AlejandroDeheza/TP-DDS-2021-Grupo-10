ed4632d7799200e02404cd953e6b6b6560f7d896
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-05-06 13:13:13 -0300

    Agrego Test Persona

--- src/test/java/modelo/persona/PersonaTest.java
+++ src/test/java/modelo/persona/PersonaTest.java
@@ -1,6 +1,20 @@
 package modelo.persona;
 
-public class PersonaTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utils.DummyData;
 
-	//TODO 1) caso de verificacion datos de contacto.
+public class PersonaTest {
+	@Test
+	@DisplayName("test getters de Persona")
+	public void creacionPersona() {
+		Persona persona = DummyData.getDummyPersona();
+		Assertions.assertSame(persona.getApellido(),DummyData.getDummyPersona().getApellido());
+		Assertions.assertSame(persona.getNombre(),DummyData.getDummyPersona().getNombre());
+		Assertions.assertEquals(persona.getFechaNacimiento(),DummyData.getDummyPersona().getFechaNacimiento());
+		Assertions.assertSame(persona.getTipoDocumento(),DummyData.getDummyPersona().getTipoDocumento());
+		Assertions.assertSame(persona.getNumeroDeDocumento(),DummyData.getDummyPersona().getNumeroDeDocumento());
+	}
+
 }
