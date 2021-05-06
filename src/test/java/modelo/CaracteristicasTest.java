4831c33595c4d61bd32979f9c4f1d325ff104146
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-05-05 23:44:59 -0300

    Changes by Emili

--- src/test/java/modelo/CaracteristicasTest.java
+++ src/test/java/modelo/CaracteristicasTest.java
@@ -12,22 +12,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class CaracteristicasTest {
-	private RepositorioCaracteristicas respositorioCaracteristicas= RepositorioCaracteristicas.getInstance();
-	private static Mascota beto;
-	private static DuenioMascota pablo;
-	private static Persona pabloPersona;
-	private static DatosDeContacto datosPablo;
-	private static DatosDeContacto datosAdmin;
-	private static Persona adminPersona;
-	private static Administrador admin;
+	  RepositorioCaracteristicas respositorioCaracteristicas= RepositorioCaracteristicas.getInstance();
+	  Mascota beto;
+	  Persona pabloPersona;
+	  DatosDeContacto datosPablo;
+	  DatosDeContacto datosAdmin;
+	  Persona adminPersona;
+	  Administrador admin;
+	  DuenioMascota duenioMascota;
 
-	@BeforeAll
-	public static void contextTest(){
-
+	@BeforeEach
+	public void contextTest(){
+		duenioMascota = crearDuenioMascota();
 		datosAdmin = new DatosDeContacto(null, "admin@mail.com");
 		adminPersona = new Persona("admin3", "admin", TipoDocumento.DNI, "1000001", datosAdmin, LocalDate.of(1995, 8, 7) );
 		admin = new Administrador("eladmin3","ElC4p0.123",adminPersona);
-
 	}
 
 	@Test
@@ -53,6 +52,18 @@ public class CaracteristicasTest {
 		listaCaracteristica.add(caracteristica);
 		beto = new Mascota(Animal.PERRO, "beto", "jp", LocalDate.of(2018,3, 4), Sexo.MACHO, "mancito", listaCaracteristica, null );
 
-		Assertions.assertThrows(ValorCaracteristicaIncompatibleException.class,()->pablo.agregarMascota(beto));
+		Assertions.assertThrows(ValorCaracteristicaIncompatibleException.class,()->duenioMascota.agregarMascota(beto));
+	}
+
+	private Mascota crearMascota(Caracteristica caracteristica) {
+		List<Caracteristica> listaCaracteristica=new ArrayList<>();
+		listaCaracteristica.add(caracteristica);
+		return new Mascota(Animal.PERRO, "beto", "jp", LocalDate.of(2018,3, 4), Sexo.MACHO, "mancito", listaCaracteristica, null );
+	}
+
+	private DuenioMascota crearDuenioMascota() {
+		datosPablo = new DatosDeContacto(null, "pablo@mail.com");
+		pabloPersona = new Persona("pablo", "Hernandez", TipoDocumento.DNI, "43212098", datosPablo, LocalDate.of(1995, 8, 7));
+		return new DuenioMascota("pepe6", "P3p3.3210", pabloPersona);
 	}
 }
