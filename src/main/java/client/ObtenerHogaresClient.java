ce70db47f2ae868ba6d151a9fdb692fdf96473ad
emazzaglia <emilianomazzaglia@gmail.com>
2021-06-21 00:39:29 -0300

    Add tests for botenerHogaresClient y ademas se modifica la estructura de los metodos que estaban en el cliente

--- src/main/java/client/ObtenerHogaresClient.java
+++ src/main/java/client/ObtenerHogaresClient.java
@@ -24,17 +24,13 @@ public class ObtenerHogaresClient {
     }
 
     public HogaresResponse obtenerHogares(String offset) throws JsonProcessingException {
-        HogaresResponse hogaresResponse = objectMapper.readValue(getClientResponse(offset),HogaresResponse.class);
-        return hogaresResponse;
-    }
-
-    public String getClientResponse(String offset) {
         Properties properties = RepositorioProperties.getInstance().getProperties();
         WebResource recurso = this.client.resource(properties.getProperty("HOGARES_API")).path(properties.getProperty("HOGARES"));
         WebResource recursoConParametros = recurso.queryParam("offset", offset);
         WebResource.Builder builder = recursoConParametros.accept(MediaType.APPLICATION_JSON).header("Authorization","Bearer " + properties.getProperty("TOKEN"));
         ClientResponse response = builder.get(ClientResponse.class);
-        return response.getEntity(String.class);
+        String responseJson = response.getEntity(String.class);
+        return objectMapper.readValue(responseJson,HogaresResponse.class);
     }
 
     public List<Hogar> obtenerTodosLosHogares() throws JsonProcessingException {
