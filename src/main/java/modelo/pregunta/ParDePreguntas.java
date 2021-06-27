6e4f467439a395a1bc44e1accbfa09309b298d2c
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-06-27 19:47:01 -0300

    Agrego package pregunta

---
+++ src/main/java/modelo/pregunta/ParDePreguntas.java
@@ -0,0 +1,31 @@
+package modelo.pregunta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParDePreguntas {
+  private String preguntaDelDador;
+  private String preguntaDelAdoptante;
+  List<ParDeRespuestas> paresDeRespuestas = new ArrayList<>();
+
+  public ParDePreguntas(String preguntaDelDador, String preguntaDelAdoptante) {
+    this.preguntaDelDador = preguntaDelDador;
+    this.preguntaDelAdoptante = preguntaDelAdoptante;
+  }
+
+  public void agregarRespuesta(ParDeRespuestas parDeRespuestas){
+    paresDeRespuestas.add(parDeRespuestas);
+  }
+
+  public String getPreguntaDelDador() {
+    return preguntaDelDador;
+  }
+
+  public String getPreguntaDelAdoptante() {
+    return preguntaDelAdoptante;
+  }
+
+  public List<ParDeRespuestas> getParesDeRespuestas() {
+    return paresDeRespuestas;
+  }
+}
---
+++ src/main/java/modelo/pregunta/ParDeRespuestas.java
@@ -0,0 +1,19 @@
+package modelo.pregunta;
+
+public class ParDeRespuestas {
+  String delDador;
+  String delAdoptante;
+
+  public ParDeRespuestas(String delDador, String delAdoptante) {
+    this.delDador = delDador;
+    this.delAdoptante = delAdoptante;
+  }
+
+  public String getDelDador() {
+    return delDador;
+  }
+
+  public String getDelAdoptante() {
+    return delAdoptante;
+  }
+}
