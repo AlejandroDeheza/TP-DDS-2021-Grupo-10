6bf00af63fe71a53d54d9361f2d81bc06dc40de5
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-07-02 19:04:37 -0300

    Arreglo test

--- src/test/java/entregaTPA3/Punto5.java
+++ src/test/java/entregaTPA3/Punto5.java
@@ -1,7 +1,11 @@
 package entregaTPA3;
 
 import modelo.adopcion.RecomendadorDeAdopciones;
+import modelo.asociacion.Asociacion;
+import modelo.mascota.Animal;
 import modelo.notificacion.NotificadorCorreo;
+import modelo.pregunta.Respuesta;
+import modelo.suscripcion.Preferencia;
 import modelo.suscripcion.SuscripcionParaAdopcion;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,7 +34,7 @@ public class Punto5 {
   }
 
   @Test
-  public void SeSugierenCincoAdopcionesEn30Segundos(){
+  public void seEnviaRecomendacionCuandoElRecomendadorRecomienda() {
     recomendador.recomendarAdopcionesASuscritos();
     // Por cada usuario suscripto deberia buscar las mascotas que hacen match y enviarle una notificacion.
     verify(notificadorCorreo, times(1)).notificar(any());
