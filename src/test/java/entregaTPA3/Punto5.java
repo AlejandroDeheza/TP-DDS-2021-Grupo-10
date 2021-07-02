d5f495e0466957c7b490d2da577c56847743b5f8
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-07-02 18:19:12 -0300

    Subo lo que tengo

--- src/test/java/entregaTPA3/Punto5.java
+++ src/test/java/entregaTPA3/Punto5.java
@@ -2,10 +2,12 @@ package entregaTPA3;
 
 import modelo.adopcion.RecomendadorDeAdopciones;
 import modelo.notificacion.NotificadorCorreo;
+import modelo.suscripcion.SuscripcionParaAdopcion;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repositorios.RepositorioDarEnAdopcion;
 import repositorios.RepositorioSuscripcionesParaAdopciones;
+import utils.DummyData;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -16,18 +18,22 @@ public class Punto5 {
   RecomendadorDeAdopciones recomendador;
   RepositorioSuscripcionesParaAdopciones repositorioSuscripcionesParaAdopciones = new RepositorioSuscripcionesParaAdopciones();
   RepositorioDarEnAdopcion repositorioDarEnAdopcion = new RepositorioDarEnAdopcion();
+  SuscripcionParaAdopcion suscripcionParaAdopcion;
 
   @BeforeEach
   public void contextLoad() {
     notificadorCorreo = mock(NotificadorCorreo.class);
+    suscripcionParaAdopcion=  mock(SuscripcionParaAdopcion.class);
+    repositorioSuscripcionesParaAdopciones.agregar(suscripcionParaAdopcion);
     recomendador = new RecomendadorDeAdopciones(5, repositorioDarEnAdopcion, repositorioSuscripcionesParaAdopciones);
+
   }
 
-  /*@Test
+  @Test
   public void SeSugierenCincoAdopcionesEn30Segundos(){
     recomendador.recomendarAdopcionesASuscritos();
     // Por cada usuario suscripto deberia buscar las mascotas que hacen match y enviarle una notificacion.
     verify(notificadorCorreo, times(1)).notificar(any());
-  }*/
+  }
 
 }
