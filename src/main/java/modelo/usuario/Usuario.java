09e5b9bf2c9366c1f53ff06ef2ff4334a4e87f98
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-11-02 19:30:47 -0300

    Refactor validador autenticacion, se asigna el objeto al atributo

--- src/main/java/modelo/usuario/Usuario.java
+++ src/main/java/modelo/usuario/Usuario.java
@@ -19,7 +19,7 @@ public class Usuario extends EntidadPersistente {
   private Persona persona;
 
   @Transient // TODO: Revisar  ValidadorAutenticacion
-  private ValidadorAutenticacion validadorAutenticacion;
+  private ValidadorAutenticacion validadorAutenticacion = new ValidadorAutenticacion();
 
   public Usuario(String usuario, String contrasenia, TipoUsuario tipo, Persona persona) {
     new ValidadorContrasenias().correrValidaciones(contrasenia);
@@ -27,7 +27,6 @@ public class Usuario extends EntidadPersistente {
     this.contrasenia = contrasenia;
     this.tipo = tipo;
     this.persona = persona;
-    this.validadorAutenticacion = new ValidadorAutenticacion();
   }
 
   public void autenticarUsuario(String contraseniaIngresada) {
