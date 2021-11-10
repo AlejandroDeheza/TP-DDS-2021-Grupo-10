3cded26911ba5bbf210bbdb6429486b0f021a1dd
AlejandroDeheza <ale.deheza@gmail.com>
2021-11-10 04:49:38 -0300

    agrego rutas y datos de prueba

--- src/main/java/main/Bootstrap.java
+++ src/main/java/main/Bootstrap.java
@@ -1,8 +1,17 @@
 package main;
 
+import modelo.notificacion.TipoNotificadorPreferido;
+import modelo.persona.DatosDeContacto;
+import modelo.persona.DocumentoIdentidad;
+import modelo.persona.Persona;
+import modelo.persona.TipoDocumento;
+import modelo.usuario.TipoUsuario;
+import modelo.usuario.Usuario;
 import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
+
+import java.time.LocalDate;
 
 public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, TransactionalOps {
 
@@ -11,7 +20,27 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
   }
 
   public void run() {
+    DocumentoIdentidad documentoIdentidad = new DocumentoIdentidad(
+        TipoDocumento.DNI,
+        "12345678"
+    );
+
+    DatosDeContacto datosDeContacto = new DatosDeContacto(
+        "12345678",
+        "pepito@gmail.com"
+    );
+
+    Persona persona = new Persona(
+        "Pepito",
+        "Gonzalez",
+        documentoIdentidad,
+        datosDeContacto,
+        LocalDate.now(),
+        TipoNotificadorPreferido.CORREO
+    );
+
     withTransaction(() -> {
+      persist(new Usuario("pepito", "pepitopepito", TipoUsuario.NORMAL, persona));
       // TODO: ver cual seria la carga inicial
     });
   }
--- src/main/java/main/Routes.java
+++ src/main/java/main/Routes.java
@@ -1,28 +1,41 @@
 package main;
 
-import controllers.HomeController;
-import controllers.SesionController;
+import controllers.*;
 import spark.Spark;
+import spark.debug.DebugScreen;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class Routes {
 
   public static void main(String[] args) {
     System.out.println("Corriendo bootstrap...");
-    //  new Bootstrap().run();
+    new Bootstrap().run();
 
     System.out.println("Iniciando servidor...");
     Spark.port(8080);
     Spark.staticFileLocation("/public");
+    DebugScreen.enableDebugScreen();
 
     HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
     HomeController homeController = new HomeController();
     SesionController sesionController = new SesionController();
+    AdminController adminController = new AdminController();
+    PublicacionesController publicacionesController = new PublicacionesController();
+    MascotasController mascotasController = new MascotasController();
 
     Spark.get("/", homeController::getHome, engine);
+
     Spark.get("/login", sesionController::mostrarLogin, engine);
     Spark.post("/login", sesionController::crearSesion);
-    Spark.get("/admin", sesionController::mostrarAdmin, engine);
+    Spark.get("/logout", sesionController::cerrarSesion);
+    Spark.get("/registracion", sesionController::mostrarRegistracion, engine);
+    Spark.post("/registracion", sesionController::registrar);
+
+    Spark.get("/admin", adminController::mostrarAdmin, engine);
+
+    Spark.get("/mascotas-en-adopcion", publicacionesController::mostrarMascotasEnAdopcion, engine);
+    Spark.get("/registracion-mascota", mascotasController::mostrarRegistracion, engine);
+
     Spark.after((request, response) -> {
       // TODO franco se los pasa
       //PerThreadEntityManagers.closeEntityManager();;
