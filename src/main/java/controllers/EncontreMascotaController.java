4931862760ea5162f6a015bb5dd1f6589323b061
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-11-10 23:56:34 -0300

    extiendo clase

--- src/main/java/controllers/EncontreMascotaController.java
+++ src/main/java/controllers/EncontreMascotaController.java
@@ -4,7 +4,7 @@ import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-public class EncontreMascotaController {
+public class EncontreMascotaController extends Controller {
 
   public ModelAndView getFormularioDatosNoUsuario(Request request, Response response) {
     if (request.session().attribute("user_id") != null) {
