18c47f50763628af09aa88d191070c66949fcd27
AlejandroDeheza <ale.deheza@gmail.com>
2021-12-03 00:14:03 -0300

    arreglo mas tests

--- src/test/java/entregaTPA4/repositorios/RepositorioCaracteristicasTest.java
+++ src/test/java/entregaTPA4/repositorios/RepositorioCaracteristicasTest.java
@@ -14,62 +14,62 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RepositorioCaracteristicasTest extends NuestraAbstractPersistenceTest {
 
-  RepositorioCaracteristicas repository = new RepositorioCaracteristicas();
+  RepositorioCaracteristicas repo = new RepositorioCaracteristicas();
 
   @Test
   @DisplayName("Agregar una caractersitica con dos valores posibles en un repositorio, aumenta el numero de caracteristicas en dos")
   public void agregarCaracteristicasConDosValoresPosiblesTest(){
     CaracteristicaConValoresPosibles caracteristicaParaAdmin = DummyData.getCaracteristicaParaAdmin();
-    repository.agregarCaracteristicasConValoresPosibles(caracteristicaParaAdmin);
-    assertEquals(caracteristicaParaAdmin.listarCaracteristicas().size(),  repository.getCaracteristicas().size());
+    repo.agregarCaracteristicasConValoresPosibles(caracteristicaParaAdmin);
+    assertEquals(caracteristicaParaAdmin.listarCaracteristicas().size(),  repo.listarTodos().size());
   }
 
   @Test
   @DisplayName("Eliminar una caractersitica con dos valores posibles en un repositorio, decrementa en dos el numero de caracteristicas")
   public void eliminarCaracteristicasConDosValoresPosiblesTest(){
     CaracteristicaConValoresPosibles caracteristicaParaAdmin = DummyData.getCaracteristicaParaAdmin();
-    repository.agregarCaracteristicasConValoresPosibles(caracteristicaParaAdmin);
-    repository.eliminarCaracteristicasConValoresPosibles(caracteristicaParaAdmin);
-    assertEquals(0,  repository.getCaracteristicas().size());
+    repo.agregarCaracteristicasConValoresPosibles(caracteristicaParaAdmin);
+    repo.eliminarCaracteristicasConValoresPosibles(caracteristicaParaAdmin);
+    assertEquals(0,  repo.listarTodos().size());
   }
 
   @Test
   @DisplayName("Si agrego una caracteristica con dos valores posibles la cantidad de caractersiticas que devuelve el repo es una")
   public void listarCaracteristicasConDosValoresPosiblesTest(){
     CaracteristicaConValoresPosibles unaCaracteristicaConDosValoresPosibles = DummyData.getCaracteristicaParaAdmin();
-    repository.agregarCaracteristicasConValoresPosibles(unaCaracteristicaConDosValoresPosibles);
-    assertEquals(1,  repository.getCaracteristicasConValoresPosibles().size());
+    repo.agregarCaracteristicasConValoresPosibles(unaCaracteristicaConDosValoresPosibles);
+    assertEquals(1,  repo.getCaracteristicasConValoresPosibles().size());
     assertEquals(unaCaracteristicaConDosValoresPosibles.getValoresCaracteristicas().get(0),
-        repository.getCaracteristicas().get(0).getValorCaracteristica());
+        repo.listarTodos().get(0).getValorCaracteristica());
     assertEquals(unaCaracteristicaConDosValoresPosibles.getValoresCaracteristicas().get(1),
-        repository.getCaracteristicas().get(1).getValorCaracteristica());
+        repo.listarTodos().get(1).getValorCaracteristica());
   }
 
   @Test
   @DisplayName("Agregar una caracteristica en un repositorio, aumenta el numero de caracteristicas en dos")
   public void agregarCaracteristicaTest(){
     Caracteristica unaCaracteristica = DummyData.getCaracteristicasParaMascota().get(0);
-    entityManager().persist(unaCaracteristica);
-    assertEquals(1,  repository.getCaracteristicas().size());
+    repo.agregar(unaCaracteristica);
+    assertEquals(1,  repo.listarTodos().size());
   }
 
   @Test
   @DisplayName("Eliminar una caracteristica en un repositorio, decrementa el numero de caracteristicas en dos")
   public void eliminarCaracteristicaTest(){
     Caracteristica unaCaracteristica = DummyData.getCaracteristicasParaMascota().get(0);
-    entityManager().persist(unaCaracteristica);
-    entityManager().remove(unaCaracteristica);
-    assertEquals(0,  repository.getCaracteristicas().size());
+    repo.agregar(unaCaracteristica);
+    repo.eliminar(unaCaracteristica);
+    assertEquals(0,  repo.listarTodos().size());
   }
 
   @Test
   @DisplayName("Listar caracteristicas me devuelve una lista con las caractersiticas agregadas")
   public void listarCaracteristicaTest(){
     Caracteristica unaCaracteristica = DummyData.getCaracteristicasParaMascota().get(0);
-    entityManager().persist(unaCaracteristica);
-    List<Caracteristica> listaFromRepo = repository.getCaracteristicas();
+    repo.agregar(unaCaracteristica);
+    List<Caracteristica> listaFromRepo = repo.listarTodos();
     assertEquals(listaFromRepo.get(0),  unaCaracteristica);
-    assertEquals(1,  repository.getCaracteristicas().size());
+    assertEquals(1,  repo.listarTodos().size());
   }
 
 }
