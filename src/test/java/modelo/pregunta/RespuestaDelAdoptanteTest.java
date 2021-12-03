8f3cc0eb5ac97580b07a7066acaaa6f0c10ad184
AlejandroDeheza <ale.deheza@gmail.com>
2021-12-03 00:16:30 -0300

    arreglo tests de preguntas

--- src/test/java/modelo/pregunta/RespuestaDelAdoptanteTest.java
+++ src/test/java/modelo/pregunta/RespuestaDelAdoptanteTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import entregaTPA4.persistencia.NuestraAbstractPersistenceTest;
+import repositorios.RepositorioParDePreguntas;
 import utils.CascadeTypeCheck;
 import utils.DummyData;
 
@@ -12,12 +13,13 @@ public class RespuestaDelAdoptanteTest extends NuestraAbstractPersistenceTest {
   ParDePreguntas parDePreguntas = DummyData.getParDePreguntas1();
   RespuestaDelAdoptante respuestaDelAdoptante = new RespuestaDelAdoptante("No", parDePreguntas);
   CascadeTypeCheck cascadeTypeCheck = new CascadeTypeCheck(respuestaDelAdoptante);
+  RepositorioParDePreguntas repositorioParDePreguntas = new RepositorioParDePreguntas();
 
   @Test
   @DisplayName("Al eliminar una RespuestaDelAdoptante, no se elimina el ParDePreguntas asociado")
   public void eliminarUnaRespuestaDelAdoptanteNoEliminaAlParDePreguntasAsociado() {
     ParDePreguntas unParDePreguntas = respuestaDelAdoptante.getParDePreguntas();
     assertTrue(cascadeTypeCheck.contemplaElCascadeType(unParDePreguntas, 1, 1, 0, 1));
-    assertEquals(unParDePreguntas.getId(), entityManager().createQuery("from ParDePreguntas", ParDePreguntas.class).getResultList().get(0).getId());
+    assertEquals(unParDePreguntas.getId(), repositorioParDePreguntas.listarTodos().get(0).getId());
   }
 }
