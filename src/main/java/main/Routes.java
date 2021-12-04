6d7f6a02dbb8a674c6fdf9d05ab0d56952636adb
maxi09 <marzm06@gmail.com>
2021-12-03 23:05:14 -0300

    Comento patitas runner

--- src/main/java/main/Routes.java
+++ src/main/java/main/Routes.java
@@ -19,7 +19,7 @@ public class Routes {
   public static void main(String[] args) {
     System.out.println("Corriendo bootstrap...");
     // new Bootstrap().run();
-    new PatitasRunner().run(); //TODO: dejo comentado asi no queda corriendo
+    //new PatitasRunner().run(); //TODO: dejo comentado asi no queda corriendo
 
     System.out.println("Iniciando servidor...");
     Spark.port(getHerokuAssignedPort());
