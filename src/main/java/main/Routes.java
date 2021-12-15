08295bfe6403867113d206704d38e48ef6f59df2
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-12-15 18:26:37 -0300

    Habilito recomendador

--- src/main/java/main/Routes.java
+++ src/main/java/main/Routes.java
@@ -20,7 +20,7 @@ public class Routes {
   public static void main(String[] args) {
 
     new Bootstrap().run();
-    //new PatitasRunner().run(); //TODO: dejo comentado asi no queda corriendo
+    new PatitasRunner().run(); //TODO: dejo comentado asi no queda corriendo
 
     System.out.println("Iniciando servidor...");
     Spark.port(getHerokuAssignedPort());
